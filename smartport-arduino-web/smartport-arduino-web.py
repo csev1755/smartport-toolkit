3f794f045f7a45a86762216c58d189fc419974e3
csev1755 <49533775+csev1755@users.noreply.github.com>
2025-12-09 07:45:19 -0600

    make controller class

--- smartport-arduino-web/smartport-arduino-web.py
+++ smartport-arduino-web/smartport-arduino-web.py
@@ -9,46 +9,51 @@ from upnp import UPnPPortMapper
 app = Flask(__name__, static_folder='static')
 socketio = SocketIO(app)
 
+class Controller:
+    def __init__(self, index, selection):
+        self.index = index
+        self.selection = selection
+        self.button_map = {
+            0: {'name': 'a', 'rok_cmd': 8},
+            1: {'name': 'b', 'rok_cmd': 9}
+        }
+        command_deck.send_command("edit", self.index, self.selection)
+
+    def input(self, input):
+        button_index = input['button']
+        pressed = input['pressed']
+        if button_index in self.button_map:
+            btn = self.button_map[button_index]
+            action = "press" if pressed else "release"
+            command_deck.send_command(action, self.index, btn['rok_cmd'])
+
 class CommandDeck:
     def __init__(self, serial_device=None):
         self.smartport = None
+        self.rok_action = {
+            "press": 0,
+            "release": 1,
+            "edit": 2,
+            "enable": 3,
+            "disable": 4,
+            "reset": 5
+        }
+
         if serial_device is not None:
             self.smartport = serial.Serial(serial_device, 115200, timeout=1)
             print(f"Connected to SmartPort device at {serial_device}")
         else:
             print("No SmartPort device specified, will only print commands for debugging")
 
-    def send_command(self, action, controller, value):
+    def send_command(self, action, controller, value=0):
         if self.smartport is not None:
-            self.smartport.write(bytes([action, controller, value]))
+            self.smartport.write(bytes([self.rok_action[action], controller, value]))
         else:
-            print(f"DEBUG - Action: {action} Controller: {controller} Value: {value}")
-
-testing_controller = 4 # VIRTUAL_CONTROLLER_1
-
-rok_action = {
-  "press": 0,
-  "release": 1,
-  "edit": 2,
-  "enable": 3,
-  "disable": 4,
-  "reset": 5
-}
-
-button_map = {
-    0: {'name': 'a', 'rok_cmd': 8},
-    1: {'name': 'b', 'rok_cmd': 9}
-}
+            print(f"DEBUG - Action: {self.rok_action[action]} Controller: {controller} Value: {value}")
 
 @socketio.on('gamepad')
 def handle_gamepad(data):
-    button_index = data['button']
-    pressed = data['pressed']
-    
-    if button_index in button_map:
-        btn = button_map[button_index]
-        action = "press" if pressed else "release"
-        command_deck.send_command(rok_action[action], testing_controller, btn['rok_cmd'])
+    controller.input(data)
 
 @app.route('/')
 def index():
@@ -61,34 +66,34 @@ def script():
 @app.route('/press', methods=['POST'])
 def press():
     data = request.json
-    command_deck.send_command(rok_action["press"], data['controller'], data['button'])
+    command_deck.send_command("press", data['controller'], data['button'])
     return "OK"
 
 @app.route('/release', methods=['POST'])
 def release():
     data = request.json
-    command_deck.send_command(rok_action["release"], data['controller'], data['button'])
+    command_deck.send_command("release", data['controller'], data['button'])
     return "OK"
 
 @app.route('/edit', methods=['POST'])
 def edit():
     data = request.json
-    command_deck.send_command(rok_action["edit"], data['controller'], data['selection'])
+    command_deck.send_command("edit", data['controller'], data['selection'])
     return "OK"
 
 @app.route('/enable', methods=['POST'])
 def enable():
-    command_deck.send_command(rok_action["enable"], request.json['controller'], 0)
+    command_deck.send_command("enable", request.json['controller'], 0)
     return "OK"
 
 @app.route('/disable', methods=['POST'])
 def disable():
-    command_deck.send_command(rok_action["disable"], request.json['controller'], 0)
+    command_deck.send_command("disable", request.json['controller'], 0)
     return "OK"
 
 @app.route('/reset', methods=['POST'])
 def reset():
-    command_deck.send_command(rok_action["reset"], 0, 0)
+    command_deck.send_command("reset", 0, 0)
     return "OK"
 
 def handle_exit(signal, frame):
@@ -106,10 +111,12 @@ if __name__ == '__main__':
     parser.add_argument('-i', '--ip', help='What IP the server will listen on', default='')
     parser.add_argument('-p', '--port', help='What port the server will listen on', default=5000)
     parser.add_argument('-u', '--upnp', help='Enable UPnP for auto port forwarding', default='')
-    
+    parser.add_argument('-c', '--controller', help='Override controller used by webserver', default=4)
+
     args = parser.parse_args()
     
     command_deck = CommandDeck(serial_device=args.device)
+    controller = Controller(4, 6)
 
     if args.upnp == "enable":
         print("Trying to open port via UPnP")
